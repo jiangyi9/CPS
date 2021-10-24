48721cb471d48f1c9f88aa2499ca04683a5e4ce9
jiangyi9 <37234222+jiangyi9@users.noreply.github.com>
2021-10-24 14:13:08 +0200

    master

--- lab-1/main.c
+++ lab-1/main.c
@@ -4,12 +4,12 @@
 
 //  This is the function to switch the the given number from decimal to binary, 
 //and the binary result is stored in the number_array[] biy by bit.
-void dec_to_bin(unsigned char number_dec, int number_array[]){
+void dec_to_bin(unsigned char number, int array[]){
     int step = 7;
-    while(number_dec != 0){
-        number_array[step] = number_dec % 2;
+    while(number != 0){
+        array[step] = number % 2;
         step--;
-        number_dec /= 2;
+        number /= 2;
     }
 }
 
@@ -19,15 +19,16 @@ void dec_to_bin(unsigned char number_dec, int number_array[]){
 int main(){
 
 
+
     //define and initiate variables
-    unsigned char number_dec, mask;
-    int number_array[8] = {};
+    unsigned char number_dec, mask_dec, mask_hex;
+    int number_array[8] = {}, mask_array[8] = {};
     int result = 0;
 
 
 
     //enter a number
-    printf("Please enter a decimal number (0 - 255): ");
+    printf("Please enter a decimal number: ");
     scanf("%d", &number_dec);
 
     //switch the the given number from decimal to binary
@@ -36,12 +37,20 @@ int main(){
 
 
     //enter the length of mask
-    printf("Please enter the length of mask (0 - 7): ");
-    scanf("%d", &mask);
+    printf("Please enter a hexadecimal mask: ");
+    scanf("%x", &mask_hex);
+
+    //switch the mask from hexadecimal to decimal
+    mask_dec = (int) mask_hex;
+
+    //switch the mask from decimal to binary
+    dec_to_bin(mask_dec, mask_array);
+
+
 
     //compute the result by judging whether the last "the length of mask" unit is 1 bit by bit
-    for(int i=0; i<mask;i++){
-        if(number_array[7-i]) result++;
+    for(int i=0; i<8;i++){
+        if(number_array[i] && mask_array[i]) result++;
     }
 
 
@@ -53,18 +62,17 @@ int main(){
     }
     printf("\n");
 
-    //print the mask (binary)
-    printf("The (binary) mask number is:  ");
+    //print the mask number (binary)
+    printf("The (binary) mask number is : ");
     for(int i=0; i<8; i++){
-        if(i<8-mask) printf("0");
-        else printf("1");
+        printf("%d", mask_array[i]);
     }
     printf("\n");
 
-    //print the result
-    printf("The result is: %d", result);
+    //print the final result
+    printf("The result is: %d\n", result);
 
 
 
-    return 0;
+return 0;
 }
